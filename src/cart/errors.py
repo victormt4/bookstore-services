f832153d6d4f61a971ba9674d138f23c869fe877
Victor Mota <victor.vmt@outlook.com>
2021-10-03 11:53:03 -0300

    Melhorando erros da api

--- src/cart/errors.py
+++ src/cart/errors.py
@@ -1,21 +1,21 @@
-from src.errors import DomainError, NotFoundError
+from src.errors import DomainError
 
 
 class OutOfStockError(DomainError):
     def __init__(self, message='Product of out stock'):
-        super().__init__(self, message)
+        super().__init__(message)
 
 
 class NotFoundOnCartError(DomainError):
     def __init__(self, message: str = 'Product not found on cart'):
-        super().__init__(self, message)
+        super().__init__(message)
 
 
 class CouponAlreadyActiveError(DomainError):
     def __init__(self, message: str = 'Coupon already active'):
-        super().__init__(self, message)
+        super().__init__(message)
 
 
 class CouponLimitError(DomainError):
     def __init__(self, message: str = 'Coupon limit exceeded'):
-        super().__init__(self, message)
+        super().__init__(message)
--- src/errors.py
+++ src/errors.py
@@ -1,7 +1,11 @@
-class DomainError(Exception):
-    pass
+from werkzeug.exceptions import BadRequest
+
+
+class DomainError(BadRequest):
+    def __init__(self, message: str):
+        super().__init__(description=message)
 
 
 class NotFoundError(DomainError):
     def __init__(self, message: str = 'Entity not found'):
-        super().__init__(self, message)
+        super().__init__(message)
