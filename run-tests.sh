d9f5e8490e3d5bab7b8ce3bc61351a603cd666be
Victor Mota <victor.vmt@outlook.com>
2022-12-02 20:07:24 -0300

    Removendo scrips obsoletos

--- run-tests.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cd docker
-docker-compose down
-docker-compose build
-docker-compose up -d
-docker-compose exec app poetry run pytest
-docker-compose down
--- start-dev-server.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cd docker
-docker-compose down
-docker-compose build
-docker-compose up -d
-docker-compose exec app flask db upgrade
-docker-compose exec app flask seed
-docker-compose up
