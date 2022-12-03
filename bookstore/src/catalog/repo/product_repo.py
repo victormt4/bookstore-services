6880a4a97af8d8c59f2665c113ca657dbc3ec231
Victor Mota <victor.vmt@outlook.com>
2022-12-03 13:58:32 -0300

    Corrigindo tipagem do ProductRepo

--- bookstore/src/catalog/repo/product_repo.py
+++ bookstore/src/catalog/repo/product_repo.py
@@ -5,11 +5,11 @@ from bookstore.src.shared.contracts.repository import Repository
 
 
 class ProductRepo(Repository[Product]):
-    def __init__(self, connection: Session):
-        self._connection = connection
+    def __init__(self, database_session: Session):
+        self._database_session = database_session
 
     def filter_by(self, **kwargs) -> list[Product]:
-        return list(self._connection.query(Product).filter_by(**kwargs))
+        return list(self._database_session.query(Product).filter_by(**kwargs))
 
     def get(self, product_id: int) -> Product | None:
-        return Product.query.get(product_id)
+        return self._database_session.query(Product).get(product_id)
